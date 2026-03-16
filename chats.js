ed18f156bfcce5380486d5fe3dc13427b5f951ed
yubhiel <yubhiel@proton.me>
2026-03-16 20:05:49 +0100

    Cat API - The button works, the cats appear

--- chats.js
+++ chats.js
@@ -7,6 +7,13 @@ btn.addEventListener('click', () => {
         .then(response => response.json())
         .then(cats => {
             console.log('nombre de chats', cats.length, 'chats', cats)
+            console.log(cats[0]);
+            let url = cats[0].url;
+            let image = document.createElement('img');
+            
+            image.setAttribute('src', url);
+            gallery.appendChild(image);
+
     });
 
 });
